990f56cf003f1e44fb96612e49e226e77fdcf0e4
dev001 <info@creativegeek.jp>
2016-03-13 06:42:05 -0400

    Fixed exporter

--- dump/export.sh
+++ dump/export.sh
@@ -3,8 +3,8 @@
 dbnames=( keywords sentences similar_keywords)
 
 #bash couchdb-backup.sh -b -H 127.0.0.1 -d my-db -f dumpedDB.json -u admin -p password
-
+rm -fr *json
 for i in "${dbnames[@]}"
 do
-	   bash couchdb-backup.sh -b -H aqueous-thicket-68007.herokuapp.com -P 80 -d $i -f $i".json"
+        bash couchdb-backup.sh -b -H aqueous-thicket-68007.herokuapp.com -P 80 -d $i -f $i".json"
 done
